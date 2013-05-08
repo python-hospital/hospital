9a10fb6f514b828984607cf6bf2aa4a173dd9ed9
Benoît Bryon <benoit@marmelune.net>
2013-05-08 22:00:03 +0200

    Added predictable health check.

--- hospital/healthchecks/__init__.py
+++ hospital/healthchecks/__init__.py
@@ -1,2 +1,15 @@
 # -*- coding: utf-8 -*-
 """Health checks for hospital project."""
+from hospital import HealthCheck
+
+
+class PredictableHealthCheck(HealthCheck):
+    """Health check that give predictable results.
+
+    Useful to check that health checks can be run, discovered, or to check
+    their output.
+
+    """
+    def test_true(self):
+        """Health checks are collected."""
+        self.assertTrue(True)

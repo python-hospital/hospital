60b25542dd0be105e655d4dec3d006805e45e2e9
Benoît Bryon <benoit@marmelune.net>
2013-05-13 08:06:26 +0200

    Introduced tests around hospital's own Python API (from hospital import HealthCheck).

---
+++ tests/api.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""Test suite around hospital's API."""
+import unittest
+
+
+class APITestCase(unittest.TestCase):
+    """Check hospital's API."""
+    def test_healthcheck_class(self):
+        """``from hospital import HealthCheck works.``"""
+        from hospital import HealthCheck

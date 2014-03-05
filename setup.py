b2638d7cc0ffed422f64a5b156fed37790054370
Benoît Bryon <benoit@marmelune.net>
2014-03-05 16:51:53 +0100

    Refs #44 - Reviewed documentation about running WSGI service. Removed 'hospital_serve' script from entry points.

--- setup.py
+++ setup.py
@@ -44,11 +44,7 @@ REQUIREMENTS = [
 if IS_PYTHON2:
     REQUIREMENTS.extend(['mock'])
 
-ENTRY_POINTS = {
-    'console_scripts': [
-        'hospital_serve = hospital.wsgi:main',
-    ]
-}
+ENTRY_POINTS = {}
 
 
 if __name__ == '__main__':  # Don't run setup() when we import this module.

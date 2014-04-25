2af12d83c93ce733ea79e25b77a356d58e1ae774
ftobia <frank.tobia@gmail.com>
2014-04-25 18:04:35 -0400

    Fix flake8 failures.

--- hospital/pytest_plugin.py
+++ hospital/pytest_plugin.py
@@ -2,13 +2,14 @@ from .core import is_healthcheck
 
 
 def pytest_configure(config):
-    """Register the "healthcheck" marker.
-    """
-    config_line = (
-        "healthcheck: mark this test as a healthcheck. "
-        "See also: http://pytest-ordering.readthedocs.org/"
-    )
-    config.addinivalue_line("markers", config_line)
+    register_healthcheck_marker(config)
+    setup_healthcheck_flag(config)
+
+
+def pytest_collection_modifyitems(session, config, items):
+    for item in items:
+        if is_healthcheck(item.function):
+            item.add_marker("healthcheck")
 
 
 def pytest_addoption(parser):
@@ -18,7 +19,17 @@ def pytest_addoption(parser):
         help="run healthchecks.")
 
 
-def pytest_configure(config):
+def register_healthcheck_marker(config):
+    """Register the "healthcheck" marker.
+    """
+    config_line = (
+        "healthcheck: mark this test as a healthcheck. "
+        "See also: http://pytest-ordering.readthedocs.org/"
+    )
+    config.addinivalue_line("markers", config_line)
+
+
+def setup_healthcheck_flag(config):
     """Implements the `--healthcheck` flag.
     `--healthcheck` flag is equivalent to `-m healthcheck`.
     """
@@ -26,9 +37,3 @@ def pytest_configure(config):
         # TODO: I didn't see a public API for this, but maybe I
         # missed something.
         setattr(config.option, "markexpr", "healthcheck")
-
-
-def pytest_collection_modifyitems(session, config, items):
-    for item in items:
-        if is_healthcheck(item.function):
-            item.add_marker("healthcheck")

383e7c402985031fdd5c02c97fafe0fbbf8cc9c2
Benoît Bryon <benoit@marmelune.net>
2014-05-02 19:06:14 +0200

    Refs #50 - Asserted pytest with -m 'not healthckeck' does not select healthchecks.

--- tests/pytest.py
+++ tests/pytest.py
@@ -5,12 +5,13 @@ import unittest
 
 class PyTestPluginTestCase(unittest.TestCase):
     """Tests around pytest integration."""
-    def test_healthcheck_marker(self):
-        "py.test -m healthcheck hospital/healthchecks/predictable runs 1 test."
+    def test_healthcheck_select(self):
+        """py.test with '-m healthcheck' selects healthchecks."""
         process = subprocess.Popen(
             [
                 'py.test',
-                '-m "healthcheck"',
+                '-m',
+                'healthcheck',
                 'hospital/healthchecks/predictable.py',
             ],
             stdout=subprocess.PIPE,
@@ -21,3 +22,21 @@ class PyTestPluginTestCase(unittest.TestCase):
         stdout = str(process.stdout.read())
         self.assertIn('collected 1 items', stdout)
         self.assertIn('===== 1 passed in ', stdout)
+
+    def test_healthcheck_deselect(self):
+        """py.test with '-m "not healthcheck" does not select healthchecks."""
+        process = subprocess.Popen(
+            [
+                'py.test',
+                '-m',
+                'not healthcheck',
+                'hospital/healthchecks/predictable.py',
+            ],
+            stdout=subprocess.PIPE,
+            stderr=subprocess.PIPE,
+        )
+        exit_code = process.wait()
+        self.assertEqual(exit_code, 0)
+        stdout = str(process.stdout.read())
+        self.assertIn('collected 1 items', stdout)
+        self.assertIn('===== 1 deselected in ', stdout)

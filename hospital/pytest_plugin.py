64d2b948726f0638242437e0ada9d7d2460d0c00
Benoît Bryon <benoit@marmelune.net>
2014-05-02 19:11:41 +0200

    Refs #50 - Removed pytest --healthcheck option: duplicate of '-m healthcheck'.

--- hospital/pytest_plugin.py
+++ hospital/pytest_plugin.py
@@ -3,20 +3,12 @@ from hospital.core import is_healthcheck
 
 def pytest_configure(config):
     register_healthcheck_marker(config)
-    setup_healthcheck_flag(config)
 
 
 def pytest_collection_modifyitems(session, config, items):
     for item in items:
         if is_healthcheck(item.cls) or is_healthcheck(item.function):
             item.add_marker("healthcheck")
-
-
-def pytest_addoption(parser):
-    parser.addoption(
-        "--healthcheck",
-        action="store_true",
-        help="run healthchecks.")
 
 
 def register_healthcheck_marker(config):
@@ -27,13 +19,3 @@ def register_healthcheck_marker(config):
         "See also: http://pytest-ordering.readthedocs.org/"
     )
     config.addinivalue_line("markers", config_line)
-
-
-def setup_healthcheck_flag(config):
-    """Implements the `--healthcheck` flag.
-    `--healthcheck` flag is equivalent to `-m healthcheck`.
-    """
-    if config.getoption("healthcheck"):
-        # TODO: I didn't see a public API for this, but maybe I
-        # missed something.
-        setattr(config.option, "markexpr", "healthcheck")
